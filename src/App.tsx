e4e90ddb34b8ee4a7215d49f47fdac427af8c681
Breno Romeiro <58960887+obrenoco@users.noreply.github.com>
2021-12-12 20:24:48 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <button className="w-full p-1 uppercase rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+      Click me
+    </button>
+  );
+}
+
+export default App;
